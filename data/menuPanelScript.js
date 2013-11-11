73144708bab0e15e9126d942184659b55f4340e6
Ivan Alagenchev <alagenchev@gmail.com>
2013-11-11 15:36:12 -0500

    dynamically populating the panel

---
+++ data/menuPanelScript.js
@@ -0,0 +1,35 @@
+var ResultEnum = 
+    {
+        "PAGE" : 0,
+        "FORM" : 1,
+        "IFRAME" : 2
+    }
+
+
+self.port.on("passwordDecisionMade", function(decision) {
+    var menuWarningArea = document.getElementById('warningDiv');
+    //console.log("decision in menu: " + JSON.stringify(decision, null, 4));
+    
+    if(!decision)
+    {
+        menuWarningArea.innerHTML = "No insecure passwords were found on this page.";
+        return;
+    }
+
+    if(decision.type == ResultEnum.PAGE)
+    {
+        menuWarningArea.innerHTML = "Insecure passwords were found on this page!";
+    }
+    else if(decision.type == ResultEnum.FORM)
+    {
+        menuWarningArea.innerHTML = "Insecure passwords were found on this form!";
+    }
+    else if(decision.type == ResultEnum.IFRAME)
+    {
+        menuWarningArea.innerHTML = "Insecure passwords were found on this iframe!";
+    }
+    menuWarningArea.innerHTML = menuWarningArea.innerHTML + " " + decision.url; 
+
+    console.log("done");
+
+});
