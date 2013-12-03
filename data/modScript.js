b36749356202b159c96d49bc00352dc2e057e9f3
Ivan Alagenchev <alagenchev@gmail.com>
2013-12-02 23:51:37 -0500

    adding logic to re-enable forms and disable buttons and password fields

--- data/modScript.js
+++ data/modScript.js
@@ -1,3 +1,24 @@
+function enableForm()
+{
+    for(var i = 0; i < document.forms.length; i++)
+    {
+
+        var elements = document.forms[i].getElementsByTagName("input");
+
+        for(var j = 0; j < elements.length; j++)
+        {
+
+            if(elements[j].disabled &&
+                (elements[j].type == "password" || elements[j].type == "submit"))
+            {
+                elements[j].disabled = false;
+                elements[j].setAttribute("style", "background-color: \"\";");
+            }
+        }
+    }
+    }
+
+self.port.on("re-enable-form", enableForm);
 function logObject(obj)
 {
     var text = JSON.stringify(obj, null, 4);
@@ -18,6 +39,12 @@ function DecisionResult(type, url)
 {
     this.type = type;
     this.url = url;
+}
+
+function setPasswordStyle(pwd)
+{
+    pwd.setAttribute("style", "background-color: rgba(239, 48, 36, .3);");
+    pwd.disabled = true;
 }
 
 var isUnsafePage = false;
@@ -53,15 +80,18 @@ for(var i = 0; i < document.forms.length; i++)
             result = new DecisionResult("page", document.URL);
             self.port.emit("getInsecurePasswordDecision", result);
             isUnsafePassword = true;
-            elements[j].setAttribute("style", "background-color: rgba(239, 48, 36, .3);");
+            setPasswordStyle(elements[j]);
         }
         else if(elements[j].type == "password" &&  isUnsafeForm)
         {
             result = new DecisionResult("form", document.forms[i].action);
             self.port.emit("getInsecurePasswordDecision", result);
             isUnsafePassword = true;
-
-            elements[j].setAttribute("style", "background-color: rgba(239, 48, 36, .3);");
+            setPasswordStyle(elements[j]);
+        }
+        else if (elements[j].type == "submit" && isUnsafePassword)
+        {
+            elements[j].disabled = true;
         }
     }
 }
