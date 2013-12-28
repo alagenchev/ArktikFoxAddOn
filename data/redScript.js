9ba36d6fd140b4b59d740633a0b328b4c4f5ec0e
Ivan Alagenchev <alagenchev@gmail.com>
2013-12-27 20:07:13 -0500

    adding logic to re-enable form

--- data/redScript.js
+++ data/redScript.js
@@ -1,3 +1,25 @@
+function enableForm()
+{
+    for(var i = 0; i < document.forms.length; i++)
+    {
+
+        var elements = document.forms[i].getElementsByTagName("input");
+
+        for(var j = 0; j < elements.length; j++)
+        {
+
+            if(elements[j].disabled &&
+                (elements[j].type == "password" || elements[j].type == "submit"))
+            {
+                elements[j].disabled = false;
+                elements[j].setAttribute("style", "background-color: \"\";");
+            }
+        }
+    }
+}
+
+self.port.on("re-enable-form", enableForm);
+
 self.port.on("insecure-password", function (decision)
 {
     for(var i = 0; i < document.forms.length; i++)
@@ -13,6 +35,11 @@ self.port.on("insecure-password", function (decision)
             if(elements[j].type == "password")
             {
                 elements[j].setAttribute("style", "background-color: rgba(239, 48, 36, .3);");
+                elements[j].disabled = true;
+            }
+            else if (elements[j].type == "submit")
+            {
+                elements[j].disabled = true;
             }
         }
     }
